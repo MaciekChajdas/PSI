4ffbd5f47df696467c550c1dde85269865fbc88b
kacper-lysiak <82179302+kacper-lysiak@users.noreply.github.com>
2022-04-23 00:04:58 +0200

    RED 35388

--- test_pair_programming.py
+++ test_pair_programming.py
@@ -12,3 +12,10 @@ def test_min():
     a = Integer(-2)
     b = Integer(5)
     assert min(a, b) == -2
+
+    
+def test_is_positive():
+    a = Integer(-2)
+    assert not Integer.isPositive(a)
+    b = Integer(5)
+    assert Integer.isPositive(b)
